be2e141241922ef254b8f6ce03802f50c90d4161
Niels Warncke <nielswarncke@porsche.digital>
2020-02-28 13:17:46 +0100

    setup script added

---
+++ setup.sh
@@ -0,0 +1,8 @@
+echo Downloading the midi dataset
+wget https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip
+unzip maestro-v2.0.0-midi.zip
+echo Creating virtual environment
+conda create -n midi-rl python=3.7
+conda activate midi-rl
+echo Installing dependencies
+pip install -r requirement.txt
