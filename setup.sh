254eaa7e72c3a8051f8cb7ac4a48ac96d4b6838f
Alessandro Schneider <alessandro.schneider@campus.tu-berlin.de>
2020-02-28 22:19:57 +0100

    fix: setup correct file

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 echo Downloading the midi dataset
-wget https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip
+curl -LO  https://storage.googleapis.com/magentadata/datasets/maestro/v2.0.0/maestro-v2.0.0-midi.zip
 unzip maestro-v2.0.0-midi.zip
 echo Creating virtual environment
 conda create -n midi-rl python=3.7
 conda activate midi-rl
 echo Installing dependencies
-pip install -r requirement.txt
+pip install -r requirements.txt
+source activate midi-rl
