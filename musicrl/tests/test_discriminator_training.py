f90dbbf9f82640095cc9fafb0fc43aa35097e3b9
Niels Warncke <nielswarncke@porsche.digital>
2020-03-06 19:35:14 +0100

    seq generator training also working now for no reason

--- musicrl/tests/test_discriminator_training.py
+++ musicrl/tests/test_discriminator_training.py
@@ -15,23 +15,23 @@ real_midis = [pretty_midi.PrettyMIDI(i) for i in filepaths]
 mapper = MidiVectorMapper(real_midis)
 
 
-# class DiscriminatorTrainingWithGeneratorsTest(unittest.TestCase):
-#     def train(self, model, train_generator, test_generator):
-#         history = model.fit_generator(train_generator(), validation_data=test_generator(),
-#                                     steps_per_epoch=1, epochs=2, validation_steps=1, validation_freq=1, verbose=1)
-#         seq_lstm.plot_history(history)
-#         self.assertTrue(len(history.history["loss"])>0)
+class DiscriminatorTrainingWithGeneratorsTest(unittest.TestCase):
+    def train(self, model, train_generator, test_generator):
+        history = model.fit_generator(train_generator(), validation_data=test_generator(),
+                                    steps_per_epoch=1, epochs=2, validation_steps=1, validation_freq=1, verbose=1)
+        seq_lstm.plot_history(history)
+        self.assertTrue(len(history.history["loss"])>0)
 
-#     def test_with_seq_lstm(self):
-#         """
-#         Test that a very short training runs without exceptions
-#         """
-#         model = seq_lstm.get_model()
-#         train_generator = RandomMidiDataGenerator(real_midis[::2], seq_lstm.make_preprocessor(mapper), mapper, 2, 100)
-#         test_generator = RandomMidiDataGenerator(real_midis[1::2], seq_lstm.make_preprocessor(mapper), mapper, 2, 100)
-#         self.train(model, train_generator, test_generator)
-#         plt.title("Seq LSTM, on generator - Accuracy")
-#         plt.savefig("test_seq_lstm_training_generator.png")
+    def test_with_seq_lstm(self):
+        """
+        Test that a very short training runs without exceptions
+        """
+        model = seq_lstm.get_model()
+        train_generator = RandomMidiDataGenerator(real_midis[::2], seq_lstm.make_preprocessor(mapper), mapper, 2, 100)
+        test_generator = RandomMidiDataGenerator(real_midis[1::2], seq_lstm.make_preprocessor(mapper), mapper, 2, 100)
+        self.train(model, train_generator, test_generator)
+        plt.title("Seq LSTM, on generator - Accuracy")
+        plt.savefig("test_seq_lstm_training_generator.png")
         
 #     def test_with_mel_lstm(self):
 #         """
@@ -44,7 +44,6 @@ mapper = MidiVectorMapper(real_midis)
 #         self.train(model, train_generator, test_generator)
 #         plt.title("Mel LSTM, on generator - Accuracy")
 #         plt.savefig("test_seq_lstm_training_generator.png")
-        
 
 class DiscriminatorTrainingWithPrecomputedData(unittest.TestCase):
     def train(self, model, train_generator, test_generator):
