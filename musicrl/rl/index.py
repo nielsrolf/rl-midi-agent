7dfc2e5d1a83b64dcd080e268e36cda1ee14ae9b
Alessandro Schneider <alessandroschneider@Alessandros-MacBook-Pro.local>
2020-03-11 16:00:45 +0100

    tmp: try to merge new env

--- musicrl/rl/index.py
+++ musicrl/rl/index.py
@@ -1,27 +1,72 @@
-#from env import *
-#env = MelEnvironment(None, mapper)
+import sys
+sys.path.append("../")
+sys.path.append("../../")
+from environment import *
+
+from tensorflow.keras.models import Sequential, load_model
+from musicrl import mel_lstm
+
+from glob import glob
+
+
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import TimeDistributed
+from tensorflow.keras import metrics
+
+import gym
+
+from musicrl.midi2vec import MidiVectorMapper
+from musicrl.render import *
+from musicrl.random_generator import generate_random_midi, resemble_midi
+from musicrl.data import RandomMidiDataGenerator
+from musicrl import mel_lstm
+
+
+filepaths = list(glob('../../maestro-v2.0.0/2008/**.midi'))
+real_midis = [pretty_midi.PrettyMIDI(i) for i in filepaths]
+mapper = MidiVectorMapper(real_midis)
+
+mapper = MidiVectorMapper(real_midis)
+real_seq = mapper.midi2vec(real_midis[1])
+real_seq.shape
+
+
+
+discriminator = load_model("../../models/mel_lstm.h5")
+env = MelEnvironment(discriminator, mel_lstm.preprocess_wav, mapper, 1000)
+
+
+notes = []
+for event in real_seq:
+    if isinstance(mapper.action2note(event), pretty_midi.Note):
+        notes.append(event)
+
+
+for i, action in enumerate(notes):
+    env.step(action)
+    if i % 500 == 0:
+        env.render()
+        plt.show()
+    if i > 500: break
+
+
+
+
 from agent import *
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 batch_size = 128
 rewards = []
 avg_rewards = []
 
-class FakeEnv:
-    def __init__(self, dim_seq):
-        self.dim_seq = dim_seq
-        self.action = np.ones(self.dim_seq)
-        self.state = self.action
-
-    def reset(self):
-        return self.state
-
-    def step(self, action):
-        self.state = np.vstack((self.state,self.action))
-        return self.state, 1, False, None
-
-env = FakeEnv(10)
 agent = DPPGAgent(env)
 
 for episode in range(50):
